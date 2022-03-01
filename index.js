acea1b0b4f5ae9828594e55ea6ebb4785c70feec
Sanad Thapa <sanadtm@gmail.com>
2022-03-01 10:05:48 -0800

    added Port

--- index.js
+++ index.js
@@ -2,28 +2,30 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const port = process.env.PORT || 5000;
+const server = http.createServer(app);
+app.set("view engine", "html");
+
+app.engine("html", require("ejs").renderFile);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/index.html");
 });
+
 app.post("/register", function (req, res) {
 	console.log(req.body);
-	res.redirect("./gamePage.html");
+	res.send(req.body);
+	const { username, nationality, gameRoomId } = req.body;
+	res.setHeader("Content-Type", "text/html");
+	res.render("gamePage.html", { username, nationality, gameRoomId });
+	res.end;
 });
-app.post("/success", function (req, res) {
-	console.log(req.body);
-	res.redirect("./gamePage.html");
-	res.end();
-});
-io.on("connection", (socket) => {
-	socket.on("chat message", (msg) => {
-		io.emit("chat message", msg);
-	});
-});
+
+// io.on("connection", (socket) => {
+// 	socket.on("chat message", (msg) => {
+// 		io.emit("chat message", msg);
+// 	});
+// });
 server.listen(port, () => {
 	console.log(`listening at http://localhost:${port}`);
 });
