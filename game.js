dd56f55b16681a2202ec3a31318f9b04339b013d
Tuan Nguyen <nguyenvutuan1@gmail.com>
2022-03-01 15:47:14 -0800

    chat-box css

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -6,8 +7,10 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/gamePage.html");
+	res.sendFile(__dirname + "/views/gamePage.html");
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on("connection", (socket) => {
 	socket.on("chat message", (msg) => {
