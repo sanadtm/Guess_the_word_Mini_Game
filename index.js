1f5bf986db52f4e4cb82af54d9ac729dcd6e9182
Sanad Thapa <sanadtm@gmail.com>
2022-03-01 18:24:01 -0800

    added CSS to index.html

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const http = require("http");
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
-app.set("view engine", "html");
-
-app.engine("html", require("ejs").renderFile);
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.get("/", (req, res) => {
@@ -16,7 +15,7 @@ app.post("/register", function (req, res) {
 	console.log(req.body);
 	res.send(req.body);
 	const { username, nationality, gameRoomId } = req.body;
-	res.setHeader("Content-Type", "text/html");
+	//res.setHeader("Content-Type", "text/html");
 	res.render("gamePage.html", { username, nationality, gameRoomId });
 	res.end;
 });
