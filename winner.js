9ec209d4549fa363318b0a54702abe4b782216c3
Sanad Thapa <sanadtm@gmail.com>
2022-03-02 21:33:01 -0800

    added the Winner Page and Winner index.js

--- winner.js
+++ winner.js
@@ -4,12 +4,19 @@ const app = express();
 const port = process.env.PORT || 5000;
 const http = require("http");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/public/views/gamePage.html");
+	res.sendFile(__dirname + "/public/views/winnerPage.html");
 });
 
 app.use(express.static(path.join(__dirname, "public")));
 
+io.on("connection", (socket) => {
+	socket.on("chat message", (msg) => {
+		io.emit("chat message", msg);
+	});
+});
 server.listen(port, () => {
 	console.log(`listening at http://localhost:${port}`);
 });
