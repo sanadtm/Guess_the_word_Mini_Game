6cf009b8cfa4ff7cf9227a5feddadfa91edca3c9
Sanad Thapa <sanadtm@gmail.com>
2022-03-05 21:15:56 -0800

    added Name of user in game page

--- index.js
+++ index.js
@@ -19,18 +19,16 @@ app.get("/", (req, res) => {
 	res.render("index.html");
 });
 let users = { name: {}, nationality: {} };
-const rooms = { name: {} };
+const rooms = {};
 
-app.get("/:room", (res, req) => {
+app.get("/:room", (req, res) => {
 	if (rooms[req.params.room] == null) {
 		return res.redirect("/");
 	}
 	res.render("room", { roomName: req.params.room });
 });
+
 app.post("/room", function (req, res) {
-	// if (rooms[req.body.room] != null) {
-	// 	res.redirect("/");
-	// }
 	rooms[req.body.room] = { users: {} };
 
 	users = req.body;
@@ -52,7 +50,37 @@ app.post("/room", function (req, res) {
 			});
 		});
 	});
+	app.post("/room", function (req, res) {
+		rooms[req.body.room] = { users: {} };
 
+		users = req.body;
+		console.log(req.body);
+		//console.log(req.body.GameID);
+		let name = req.body.name;
+
+		res.render("gamePage.html", { uname: name, users: users, rooms: rooms });
+		//res.json({ name1: req.body.name });
+		io.emit("user-joined", req.body);
+
+		io.on("connection", (socket) => {
+			socket.on("chat message", (msg) => {
+				io.emit("chat message", req.body.name + " :: " + msg);
+				console.log("Rooms " + Object.keys(socket.rooms));
+				console.log("Rooms " + socket.rooms);
+				socket.on("disconnect", () => {
+					socket.emit("chat message", req.body.name + ":: Disconnected");
+				});
+			});
+		});
+
+		io.on("connection", (socket) => {
+			socket.emit("chat message", req.body.name + ":: Connected");
+
+			socket.on("disconnect", () => {
+				socket.emit("chat message", req.body.name + ":: Disconnected");
+			});
+		});
+	});
 	io.on("connection", (socket) => {
 		socket.emit("chat message", req.body.name + ":: Connected");
 
