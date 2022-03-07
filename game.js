665de53a830d448af692b0ac339653a420da06d7
Sanad Thapa <sanadtm@gmail.com>
2022-03-06 21:35:46 -0800

    Working Chat

--- game.js
+++ game.js
@@ -1,16 +1,31 @@
-// const express = require("express");
-// const http = require("http");
-// const app = express();
-// const { Server } = require("socket.io");
-// const server = http.createServer(app);
-// const io = new Server(server);
-// const path = require("path");
-// global.count = 10;
-// const port = process.env.PORT || 4000;
-// app.use(express.static(path.join(__dirname, "public")));
-
+const express = require("express");
+const http = require("http");
+const app = express();
+const { Server } = require("socket.io");
+const server = http.createServer(app);
+const io = new Server(server);
+const path = require("path");
+global.count = 10;
+const port = process.env.PORT || 4000;
+app.use(express.static(path.join(__dirname, "public")));
+const name = "sanad";
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/public/views/gamePage.html");
+});
+
+io.on("connection", (socket) => {
+	console.log("A user connected");
+	socket.broadcast.emit("message", "A USER has joined");
+
+	socket.on("chat message", (msg) => {
+		io.emit("chat message", name + " ::" + msg);
+	});
+
+	socket.on("disconnect", () => {
+		console.log("user disconnected");
+
+		io.emit("message", "A USER has LEFT!!");
+	});
 });
 
 server.listen(port, () => {
--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const { Server } = require("socket.io");
 const server = http.createServer(app);
 const io = new Server(server);
 const path = require("path");
-global.count = 10;
 const port = process.env.PORT || 4000;
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -18,37 +17,20 @@ app.use(express.json());
 app.get("/", (req, res) => {
 	res.render("index.html");
 });
-let user = { name: {}, nationality: {} };
-const rooms = {};
-app.get("/:room", (req, res) => {
-	console.log(req.body);
-	res.render("gamePage.html", { roomName: req.params.room });
-});
-
+let name = [];
+let eachname;
+let connectCounter = 0;
+let userData = [];
+let userArray = [];
+let rooms = {};
 app.post("/room", function (req, res) {
 	const { username, nationality, room } = req.body;
-
+	userData.push(req.body);
 	rooms[req.body.room] = { users: {} };
-	users = req.body;
+	eachname = req.body.name;
 	//console.log(req.body);
-	let name = req.body.name;
-	res.render("gamePage.html", { uname: name, users: users, rooms: rooms, cnt: 1 });
-
-	io.on("connection", (socket) => {
-		console.log("A user connected");
-		console.log(user);
-		socket.broadcast.emit("message", "A USER has joined");
-
-		socket.on("chat message", (msg) => {
-			io.emit("chat message", name + " ::" + msg);
-		});
-
-		socket.on("disconnect", () => {
-			console.log("user disconnected");
-			console.log(user);
-			io.emit("message", "A USER has LEFT!!");
-		});
-	});
+	name.push(req.body.name);
+	res.render("gamePage.html", { uname: name, userData: userData, rooms: rooms, cnt: 1 });
 
 	//res.json({ name1: req.body.name });
 	// io.emit("user-joined", req.body);
@@ -64,26 +46,41 @@ app.post("/room", function (req, res) {
 	// 	});
 	// });
 
-	// io.on("connection", (socket) => {
-	// 	socket.emit("chat message", req.body.name + ":: Connected");
-
 	// 	socket.on("disconnect", () => {
 	// 		socket.emit("chat message", req.body.name + ":: Disconnected");
 	// 	});
 	// });
-});
-app.use((req, res, next) => {
-	const err = new Error("Not Found");
-	err.status = 404;
-	next(err);
+	++connectCounter;
 });
 
-app.use((err, req, res, next) => {
-	res.locals.error = err;
-	const status = err.status || 500;
-	res.status(status);
-	//res.render("error");
+io.on("connection", (socket) => {
+	console.log("A user connected");
+	socket.broadcast.emit("message", "A USER has joined");
+	socket.name = eachname;
+	socket.on("chat message", (msg) => {
+		io.emit("chat message", socket.name + " ::" + msg);
+	});
+
+	io.on("connection", (socket) => {
+		socket.emit("chat message", socket.name + ":: Connected");
+
+		socket.on("disconnect", () => {
+			socket.emit("chat message", socket.name + ":: Disconnected");
+		});
+	});
 });
+// app.use((req, res, next) => {
+// 	const err = new Error("Not Found");
+// 	err.status = 404;
+// 	next(err);
+// });
+
+// app.use((err, req, res, next) => {
+// 	res.locals.error = err;
+// 	const status = err.status || 500;
+// 	res.status(status);
+// 	//res.render("error");
+// });
 server.listen(port, () => {
 	console.log(`listening at http://localhost:${port}`);
 });
