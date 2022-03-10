81e4fff776784318cdc811771688d74fe36fc16b
Sanad Thapa <sanadtm@gmail.com>
2022-03-10 09:56:52 -0800

    Added Responsive to winner page after deployment

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const { Server } = require("socket.io");
 const server = http.createServer(app);
 const io = new Server(server);
 const path = require("path");
-const { SocketAddress } = require("net");
 const port = process.env.PORT || 4000;
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -22,7 +21,7 @@ app.get("/", (req, res) => {
 let name_points = {};
 let eachname;
 let name;
-let userData = []; //hwo
+let userData = [];
 let onlineUsers = [];
 let rooms = {};
 let userGetPointsArr = [];
@@ -31,8 +30,7 @@ app.post("/room", function (req, res) {
 	const { username, nationality, room } = req.body;
 	rooms[req.body.room] = { users: {} };
 	eachname = req.body.name;
-	//console.log(req.body);
-	//console.log(userData);
+
 	onlineUsers.push(eachname);
 
 	name_points = { name: req.body.name, points: 0 };
@@ -43,12 +41,10 @@ app.post("/room", function (req, res) {
 
 io.on("connection", (socket) => {
 	socket.on("ary-answer", (answerArray) => {
-		//console.log(answerArray);
 		io.emit("ary-answer", answerArray);
 	});
 
 	socket.on("country-answer", (answerArrayCountry) => {
-		//console.log(answerArray);
 		io.emit("country-answer", answerArrayCountry);
 	});
 
@@ -60,12 +56,8 @@ io.on("connection", (socket) => {
 	socket.on("user get points", (userGetPoints) => {
 		userGetPointsArr.push(userGetPoints);
 		let combineArr = userGetPointsArr.concat(userGetPoints);
-		/* console.log("line 51", userGetPoints);
-		console.log("line 52", userGetPointsArr);
-		console.log("line 54", combineArr); */
 	});
 
-	//	io.emit("displayUsers", name_points);
 	io.emit("send_allUsers1", userData);
 	console.log("line 50", userData);
 
@@ -79,59 +71,35 @@ io.on("connection", (socket) => {
 
 	io.emit("singleUser", eachname);
 
-	/* socket.on('testing', (test) => {
-		io.emit('testing', socket.name + test );
-		console.log("line 70", socket.name);
-	}); */
-
-	//sending the name of current player
-	//let curr = socket.name;
-	//console.log("curr: ", curr);
 	console.log("current player ", socket.name);
 	io.emit("current Player", onlineUsers);
-
-	//console.log("socket ID ::" + socket.client.id);
 	socket.join("chatroom");
-	// socket.on("displayUsers", (username) => {
-	// 	io.emit("displayUsers", " :: " + username);
-	// });
 
-	//console.log;
-	console.log("111", name_points);
-
-	//console.log("User Data" + userData.name);
-	/* socket.on("send_allUsers", function (ALLPlayers) {
-		console.log("server test : ", ALLPlayers);
-		io.emit("send_allUsers1", ALLPlayers);
-	});
- */
-	// if (userData.length === 0) {
-	// //	userData.push(socket.client.id);
-	// } else {
-	// 	userData.forEach((socketID) => {
-	// 		if (socket.client.id === socketID) {
-	// 			//display previous users
-	// 		} else {
-	// 			//io.emit("displayPrevUsers", userData);
-	// 			socket.on("send_allUsers12", function (data) {
-	// 				if (data.name || data.points) {
-	// 					console.log("Server :: " + data);
-	// 					io.emit("displayPrevUsers", data);
-	// 				}
-	// 			});
-	// 		}
-	// 	});
-	// }
+	// testing user data
+	if (userData.length === 0) {
+	} else {
+		userData.forEach((socketID) => {
+			if (socket.client.id === socketID) {
+			} else {
+				socket.on("send_allUsers12", function (data) {
+					if (data.name || data.points) {
+						console.log("Server :: " + data);
+						io.emit("displayPrevUsers", data);
+					}
+				});
+			}
+		});
+	}
 	socket.broadcast.emit("chat message", socket.name + ":: Connected");
 	socket.on("disconnect", () => {
 		socket.broadcast.emit("chat message", socket.name + ":: Disconnected");
 	});
 
 	io.to("chatroom").emit("usersInRoom", socket.name);
-	// io.emit("displayUsers", userData);
 });
 
 // server.listen(port, () => {
 // 	console.log(`listening at http://localhost:${port}`);
-//  }); heroku game
+// });
+//heroku game server
 server.listen(process.env.PORT || port);
